92bbf849051713c120208b8c2f60ee08a77f2eeb
Svitlana Oliinyk <Svitlana Oliinyk>
2017-04-19 22:36:58 +0300

    modify object

--- settings/pulse_api_client.py
+++ settings/pulse_api_client.py
@@ -22,8 +22,11 @@ class PulseRestAPI:
         ob = requests.get(self.url + str(obj['id']) + '/')
         return ob
 
-    def modify_obj(self, obj):
-        pass
+    def modify_obj(self, obj, obj_mod):
+        mod_data = obj_mod.get_dict()
+        requests.put(self.url + str(obj['id']) + "/", data=mod_data)
+        get_obj = requests.get(self.url + str(obj['id']) + '/')
+        return get_obj
 
     def delete_obj(self, obj):
         del_book = requests.delete(self.url + str(obj.id) + '/')
--- tests/test_create_book.py
+++ tests/test_create_book.py
@@ -13,10 +13,10 @@ for item in old:
     test_data.append(book)
 
 
-# test_data = [
-#     Book(title='Пляшущие человечки', author='К.Дойль'),
-#     Book(title='S', author='O'),
-# ]
+test_data_modify = [
+    Book(title='Пляшущие измененные', author='Измененный'),
+    Book(title='SSSS', author='OOOO'),
+]
 
 @pytest.mark.parametrize('book', test_data, ids=[repr(b) for b in test_data])
 def test_new_book(app, book):
@@ -35,6 +35,17 @@ def test_book_in_list(app, book):
     assert new_book in book_list
     app.delete_obj(book)
 
+
+@pytest.mark.parametrize('book', test_data, ids=[repr(b) for b in test_data])
+@pytest.mark.parametrize('book_mod', test_data_modify, ids=[repr(k) for k in test_data_modify])
+def test_modify_book(app, book, book_mod):
+
+    new_book = app.create_obj(book).json()
+    mod_book = app.modify_obj(new_book, book_mod)
+    #Verification
+    assert mod_book.status_code == 200
+    # assert new_book.json() == book.get_dict()
+    app.delete_obj(book)
 
 def test_new_book_without_autor(app):
     book = Book(title='Пляшущие человечки')
